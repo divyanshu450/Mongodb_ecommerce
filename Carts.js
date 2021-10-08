0b35f1bea9ccb44dcf3d5812c24506725b088714
Divyanshu Kumar <divyanshu.kumar450@gmail.com>
2021-10-08 10:11:50 +0530

    added roles to users cart

--- Carts.js
+++ Carts.js
@@ -4,18 +4,18 @@ MongoClient.connect(url , function(err, client)
 {
     if(err) throw err
     var cartobj=[
-        {Product:'Laptops', User: 'vb001', Product_Qty:2, Base_Price:829.30, Sell_Price:1020.30, Total_Price:1050.50},
-        {Product:'Smartphones', User: 'vb002', Product_Qty:2, Base_Price:819.30, Sell_Price:1100.30, Total_Price:1250.50},
-        {Product:'Accessories', User: 'vb006', Product_Qty:2, Base_Price:629.30, Sell_Price:750.30, Total_Price:780.50},
-        {Product:'Shoes', User: 'vb001', Product_Qty:1, Base_Price:329.30, Sell_Price:420.30, Total_Price:490.50},
-        {Product:'Laptops', User: 'vb001', Product_Qty:2, Base_Price:829.30, Sell_Price:1020.30, Total_Price:1050.50},
-        {Product:'Smartphones', User: 'vb002', Product_Qty:2, Base_Price:819.30, Sell_Price:1100.30, Total_Price:1250.50},
-        {Product:'Accessories', User: 'vb006', Product_Qty:2, Base_Price:629.30, Sell_Price:750.30, Total_Price:780.50},
-        {Product:'Shoes', User: 'vb001', Product_Qty:1, Base_Price:329.30, Sell_Price:420.30, Total_Price:490.50},
-        {Product:'Laptops', User: 'vb001', Product_Qty:2, Base_Price:829.30, Sell_Price:1020.30, Total_Price:1050.50},
-        {Product:'Smartphones', User: 'vb002', Product_Qty:2, Base_Price:819.30, Sell_Price:1100.30, Total_Price:1250.50},
-        {Product:'Accessories', User: 'vb006', Product_Qty:2, Base_Price:629.30, Sell_Price:750.30, Total_Price:780.50},
-        {Product:'Shoes', User: 'vb001', Product_Qty:1, Base_Price:329.30, Sell_Price:420.30, Total_Price:490.50},
+        {Product:'Laptops', User: 'vb001', Product_Qty:2, Base_Price:829.30, Sell_Price:1020.30, Total_Price:1050.50, roles:'dispatching items'},
+        {Product:'Smartphones', User: 'vb002', Product_Qty:2, Base_Price:819.30, Sell_Price:1100.30, Total_Price:1250.50, roles:'waiting for confirmation'},
+        {Product:'Accessories', User: 'vb006', Product_Qty:2, Base_Price:629.30, Sell_Price:750.30, Total_Price:780.50, roles:'verifying payment'},
+        {Product:'Shoes', User: 'vb001', Product_Qty:1, Base_Price:329.30, Sell_Price:420.30, Total_Price:490.50, roles:'checking transactions'},
+        {Product:'Laptops', User: 'vb001', Product_Qty:2, Base_Price:829.30, Sell_Price:1020.30, Total_Price:1050.50, roles:'checking configuration'},
+        {Product:'Smartphones', User: 'vb002', Product_Qty:2, Base_Price:819.30, Sell_Price:1100.30, Total_Price:1250.50,roles:'placing order'},
+        {Product:'Accessories', User: 'vb006', Product_Qty:2, Base_Price:629.30, Sell_Price:750.30, Total_Price:780.50, roles:'canceling order'},
+        {Product:'Shoes', User: 'vb001', Product_Qty:1, Base_Price:329.30, Sell_Price:420.30, Total_Price:490.50, roles:'checking transaction'},
+        {Product:'Laptops', User: 'vb001', Product_Qty:2, Base_Price:829.30, Sell_Price:1020.30, Total_Price:1050.50, roles:'checking for extra discount'},
+        {Product:'Smartphones', User: 'vb002', Product_Qty:2, Base_Price:819.30, Sell_Price:1100.30, Total_Price:1250.50, roles:'dispatching'},
+        {Product:'Accessories', User: 'vb006', Product_Qty:2, Base_Price:629.30, Sell_Price:750.30, Total_Price:780.50, roles:'counting items'},
+        {Product:'Shoes', User: 'vb001', Product_Qty:1, Base_Price:329.30, Sell_Price:420.30, Total_Price:490.50, roles:'placing order'},
     ];
 
     const db=client.db("ecommerce")
